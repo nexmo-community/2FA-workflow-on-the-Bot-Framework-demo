2d91406e83b48ae4514828b4145f3dd79e3146ca
BiBi <othmani.rabeb@live.fr>
2017-12-13 15:16:50 +0000

    Customised Prompts for the dialog flow

--- 2FABotDemo/2FABotDemo/UserProfile.cs
+++ 2FABotDemo/2FABotDemo/UserProfile.cs
@@ -9,8 +9,11 @@ namespace _2FABotDemo
     [Serializable]
     public class UserProfile
     {
+        [Prompt("What's your first name?")]
         public string FirstName;
+        [Prompt("How about your last name?")]
         public string LastName;
+        [Prompt("I now need your phone number in its international format but without '+' or '00'. I will shortly send a verification code.")]
         public string PhoneNumber;
         public static IForm<UserProfile> BuildForm()
         { 
